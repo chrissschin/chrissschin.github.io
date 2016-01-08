11e8e1876ef868314210e56096a05b1b066f97c7
chrissschin <solischris92@gmail.com>
2016-01-08 01:40:21 -0800

    did layout

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+'use-strict'
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    neat = require('node-neat').includePaths;
+var browserSync = require('browser-sync').create();
+
+var paths = {
+    scss: './scss/*.scss',
+    html: './*.html'
+};
+
+// Static server + watch scss/html files
+gulp.task('serve', ['sass'], function() {
+
+  browserSync.init({
+    server: './'
+  });
+
+  gulp.watch(paths.scss, ['sass']);
+  gulp.watch(paths.html).on('change', browserSync.reload);
+
+});
+
+//compile sass
+gulp.task('sass', function () {
+    return gulp.src(paths.scss)
+        .pipe(sass({
+            includePaths: ['sass'].concat(neat)
+        }))
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
