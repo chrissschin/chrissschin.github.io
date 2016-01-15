c5b9e6b58ab4ad630905729ac4ef8604987f909c
Christian Solis <Christiansolis@Christian-Solis-Macbook.local>
2016-01-14 16:56:41 -0800

    added animations

--- myApp.js
+++ myApp.js
@@ -1,4 +1,22 @@
-angular.module('myApp', ['ui.router'])
+angular.module('myApp', ['ui.router','ngAnimate'])
   .config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise('/home')
-  })
+
+    $stateProvider
+      .state('home', {
+        url : '/home',
+        templateUrl : 'views/home.html'
+      })
+      .state('about', {
+        url : '/about',
+        templateUrl : 'views/about.html'
+      })
+      .state('work', {
+        url : '/work',
+        templateUrl : 'views/work.html'
+      })
+      .state('contact', {
+        url : '/contact',
+        templateUrl : 'views/contact.html'
+      })
+  });
