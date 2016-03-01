df3c1797ea90107dc0d31232bce7534a93a1750e
chrissschin <solischris92@gmail.com>
2016-02-29 19:40:30 -0800

    added show more link

---
+++ main.js
@@ -0,0 +1,11 @@
+
+$(document).ready(function(){
+    
+    $('.see-more').on("click", function(){
+        
+        $('.hide').removeClass('hide');
+        $('.show').addClass('hide');
+        
+    });
+    
+});
