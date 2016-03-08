3642a72799a817eef94ec6c5d88eee0a2d81b6c6
chrissschin <solischris92@gmail.com>
2016-03-08 01:11:05 -0800

    added footer animation

--- main.js
+++ main.js
@@ -1,11 +1,35 @@
 
 $(document).ready(function(){
     
-    $('.see-more').on("click", function(){
+    var footer = $('#footer')
+    var extra = 80;
+
+    footer.css({ opacity: '0', display: 'block' });
+
+    $(window).scroll(function() {
         
-        $('.hide').removeClass('hide');
-        $('.show').addClass('hide');
+    var scrolledLength = ( $(window).height() + extra ) + $(window).scrollTop();
+    var documentHeight = $(document).height();
+
+  
+
+
+        console.log( 'Scroll length: ' + scrolledLength + ' Document height: ' + documentHeight );
         
+            
+    if( scrolledLength >= documentHeight ) {
+        
+        footer
+            .addClass('bottom')
+            .stop().animate({ bottom: '0', opacity: '1' }, 300);
+
+    }
+    else if ( scrolledLength <= documentHeight && footer.hasClass('bottom') ) {           
+            footer
+            .removeClass('bottom')
+            .stop().animate({ bottom: '-100', opacity: '0' }, 300);
+
+    } 
     });
     
 });
