98579a1c731d0f4d78411e202167b1b29120b2ee
chrissschin <solischris92@gmail.com>
2016-03-22 01:40:26 -0700

    fixed layout again

--- main.js
+++ main.js
@@ -1,35 +1,35 @@
 
 $(document).ready(function(){
-    
+
     var footer = $('#footer')
-    var extra = 80;
+    var extra = 35;
 
     footer.css({ opacity: '0', display: 'block' });
 
     $(window).scroll(function() {
-        
+
     var scrolledLength = ( $(window).height() + extra ) + $(window).scrollTop();
     var documentHeight = $(document).height();
 
-  
+
 
 
         console.log( 'Scroll length: ' + scrolledLength + ' Document height: ' + documentHeight );
-        
-            
+
+
     if( scrolledLength >= documentHeight ) {
-        
+
         footer
             .addClass('bottom')
             .stop().animate({ bottom: '0', opacity: '1' }, 300);
 
     }
-    else if ( scrolledLength <= documentHeight && footer.hasClass('bottom') ) {           
+    else if ( scrolledLength <= documentHeight && footer.hasClass('bottom') ) {
             footer
             .removeClass('bottom')
             .stop().animate({ bottom: '-100', opacity: '0' }, 300);
 
-    } 
+    }
     });
-    
+
 });
